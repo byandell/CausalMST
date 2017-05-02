62ffdec41b2fd3c679c3e698b516a72cf0e6f6c7
Brian Yandell <byandell@wisc.edu>
2017-05-01 21:13:07 -0500

    add driver, target, mediator name options to plot_med_scatter

--- R/med_scatter.R
+++ R/med_scatter.R
@@ -1,12 +1,122 @@
-med_scatter <- function(driver, target, mediator, fitFunction,
-                        kinship, cov_tar, cov_med) {
+#' Scatter plot for mediator and target
+#' 
+#' @param driver vector or matrix with driver values
+#' @param target vector or 1-column matrix with target values
+#' @param mediator vector or 1-column matrix with mediator values
+#' @param kinship optional kinship matrix among individuals
+#' @param cov_tar optional covariates for target
+#' @param cov_med optional covariates for mediator
+#' @param fitFunction function to fit models with driver, target and mediator
+#' @param sdp SNP distribution pattern for plot colors
+#' @param allele Driver has alleles if \code{TRUE}, otherwise allele pairs.
+#' 
+#' @export
+#' 
+#' @importFrom CCSanger sdp_to_logical
+#' @importFrom stringr str_split
+#' @importFrom ggplot2 aes autoplot facet_wrap geom_hline geom_smooth geom_text ggplot ggtitle xlab ylab
+#' 
+med_scatter <- function(driver, target, mediator,
+                        kinship, cov_tar, cov_med, fitFunction,
+                        sdp = 32,
+                        allele = TRUE) {
 
-  
   commons <- common_data(driver, target, mediator,
                          kinship, cov_tar, cov_med)
   
   cov_names <- names(cov_med)[!(names(cov_med) %in% names(cov_tar))]
+  
+  for(i in c("target","mediator"))
+    colnames(commons[[i]]) <- i
 
-  data.frame(commons$driver, commons$target, commons$mediator,
-               commons$cov_tar, commons$cov_med[,cov_names, drop = FALSE])
+  dat <- data.frame(commons$driver, commons$target, commons$mediator,
+                    commons$cov_tar, commons$cov_med[,cov_names, drop = FALSE])
+  
+  genos <- colnames(commons$driver)
+  if(allele)
+    haplos <- genos
+  else
+    haplos <- unique(unlist(stringr::str_split(genos, "")))
+  
+  alt <- haplos[CCSanger::sdp_to_logical(sdp)]
+  if(allele) {
+    dat$geno <- apply(round(2 * commons$driver), 1, 
+                      function(x)
+                        paste(rep(genos, x), collapse = ""))
+    dat$alt <- factor(round(2 * apply(dat[, alt, drop = FALSE], 1, sum)))
+  } else {
+    dat$geno <- apply(commons$driver, 1, 
+                      function(x) genos[which.max(x)])
+    dat$alt <- sapply(stringr::str_split(genos, ""), 
+                      function(x, alt) sum(x %in% alt), alt)
+  }
+  dat$Sex <- c("Female", "Male")[1 + dat$sex]
+  
+  # Fit target and target|mediator models
+  fit <- med_fits(driver, target, mediator,
+                  fitFunction, kinship, cov_tar, cov_med)
+  for(i in names(fit$coef)[1:2]) {
+    tmp <- fit$coef[[i]][1:8]
+    dat[[i]] <- c(as.matrix(dat[names(tmp)]) %*% tmp)
+  }
+  
+  class(dat) <- c("med_scatter", class(dat))
+  
+  dat
 }
+#' @export
+plot_med_scatter <- function(x, ..., 
+                             type = c("by_mediator", "by_target", "driver_offset", "driver"),
+                             tname = "target", mname = "mediator", dname = "driver") {
+  
+  type <- match.arg(type)
+  
+  p <- ggplot2::ggplot(x) +
+    ggplot2::aes(label = geno, col = alt)
+
+  switch(type,
+         by_mediator = {
+           main <- paste(tname, "vs", mname, "by sex and", dname)
+           p <- p + 
+             ggplot2::aes(mediator, target) +
+             ggplot2::xlab(mname) +
+             ggplot2::ylab(tname)
+         },
+         by_target = {
+           main <- paste(mname, "vs", tname, "by sex and", dname)
+           p <- p + 
+             ggplot2::aes(target, mediator) +
+             ggplot2::xlab(tname) +
+             ggplot2::ylab(mname)
+         },
+         driver_offset = {
+           main <- paste("mediation of", dname, "effect on ", tname)
+           p <- p + 
+             ggplot2::aes(mediator, t.d_t - t.md_t.m) +
+             ggplot2::xlab(mname) +
+             ggplot2::ylab(paste(dname, "effect offset"))
+         },
+         driver = {
+           main <- paste("mediation offset of", dname, "effect vs", dname, "effect")
+           p <- p + 
+             ggplot2::aes(t.d_t, t.d_t - t.md_t.m) +
+             ggplot2::xlab(paste(dname, "effect")) +
+             ggplot2::ylab(paste(dname, "effect offset"))
+         })
+
+  p + 
+    ggplot2::geom_hline(yintercept = 0) +
+    ggplot2::geom_smooth(method = "lm", se=FALSE) +
+    ggplot2::geom_text(size=3) +
+    ggplot2::facet_wrap(~ Sex) +
+    ggplot2::ggtitle(main)
+}
+#' @export
+autoplot.med_scatter <- function(x, ...) {
+  plot_med_scatter(x, ...)
+}
+#' @export
+plot.med_scatter <- function(x, ...) {
+  ggplot2::autoplot(x, ...)
+}
+
